5027834655d0f0f2f104ed4012b77cee2f4372b5
abenettt <me@abenet.email>
2021-01-12 07:34:55 +0100

    Add more explicit example test

--- tests/AutoReview/SrcCodeTest.php
+++ tests/AutoReview/SrcCodeTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\AutoReview;
 
+use Ergebnis\Classy\Construct;
+use Ergebnis\Classy\Constructs;
 use Ergebnis\Test\Util\Helper;
 use PHPUnit\Framework\TestCase;
 
@@ -12,5 +14,24 @@ final class SrcCodeTest extends TestCase
     public function testSrcClassesAreAbstractOrFinal(): void
     {
         self::assertClassesAreAbstractOrFinal(__DIR__ . '/../../src');
+    }
+
+    /** @dataProvider constructProvider */
+    public function testConstructsExist(Construct $construct): void
+    {
+        $name = $construct->name();
+        self::assertTrue(class_exists($name) || trait_exists($name) || interface_exists($name) || file_exists($name));
+    }
+
+    /** @return \Generator<Construct> */
+    public function constructProvider(): \Generator
+    {
+        $constructs = Constructs::fromDirectory(__DIR__ . '/../../src');
+
+        foreach ($constructs as $construct) {
+            yield $construct->name() => [
+                $construct,
+            ];
+        }
     }
 }
